aa3e370b8b148a3bdfc03abe6f85efc2bb300e67
unknown <sheikhalnahian@gmail.com>
2025-07-30 14:38:44 +0600

    break statement technique is practised

---
+++ break.js
@@ -0,0 +1,26 @@
+// use of break in the loop, we break a loop using break statement. let see
+
+for(let i = 0; i<=20; i++)
+{
+    // console.log(i);
+
+    if(i>10)
+    {
+        break;
+    }
+    console.log(i);
+}
+
+console.log('break in while loop')
+let n = 0;
+
+while(n<=20)
+{
+    // console.log(n);
+    if(n>10)
+    {
+        break;
+    }
+    console.log(n);
+    n++;
+}
